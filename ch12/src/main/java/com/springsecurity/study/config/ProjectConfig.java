a70f3c9d5b6a708612c10eab71400f3d98aed562
illinia <seolin1218@gmail.com>
2022-09-28 17:37:49 +0900

    OAuth2 가 작동하는 법

---
+++ ch12/src/main/java/com/springsecurity/study/config/ProjectConfig.java
@@ -0,0 +1,21 @@
+package com.springsecurity.study.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.oauth2Login();
+
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
---
+++ ch12/src/main/java/com/springsecurity/study/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.springsecurity.study.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String main(OAuth2AuthenticationToken token) {
+        log.info(String.valueOf(token.getPrincipal()));
+        return "main.html";
+    }
+}
