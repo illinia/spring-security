548405af7fbdee1bea414396fcb570a76a0d108e
illinia <seolin1218@gmail.com>
2022-09-29 14:14:04 +0900

    리소스 서버 구현

--- ch14/ch14-ex1-as/src/main/java/com/springsecurity/study/config/AuthServerConfig.java
+++ ch14/ch14-ex1-as/src/main/java/com/springsecurity/study/config/AuthServerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -16,30 +17,23 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     private AuthenticationManager authenticationManager;
 
     @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client")
+                .secret("secret")
+                .authorizedGrantTypes("password", "refresh_token")
+                .scopes("read")
+                .and()
+                .withClient("resourceserver")
+                .secret("resourceserversecret");
+    }
+
+    @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(authenticationManager);
     }
 
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-//        var service = new InMemoryClientDetailsService();
-//
-//        var cd = new BaseClientDetails();
-//        cd.setClientId("client");
-//        cd.setClientSecret("secret");
-//        cd.setScope(List.of("read"));
-//        cd.setAuthorizedGrantTypes(List.of("password"));
-//
-//        service.setClientDetailsStore(
-//                Map.of("client", cd)
-//        );
-//
-//        clients.withClientDetails(service);
-
-        clients.inMemory()
-                .withClient("client")
-                .secret("secret")
-                .authorizedGrantTypes("password")
-                .scopes("read");
+    public void configure(AuthorizationServerSecurityConfigurer security) {
+        security.checkTokenAccess("isAuthenticated()");
     }
 }
---
+++ ch14/ch14-ex1-rs/src/main/java/com/springsecurity/study/config/ResourceServerConfig.java
@@ -0,0 +1,9 @@
+package com.springsecurity.study.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig {
+}
